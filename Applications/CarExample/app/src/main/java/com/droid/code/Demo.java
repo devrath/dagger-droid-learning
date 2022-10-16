b68d6301db05b313044826fff723b8313d1c491e
Devrath <devrath@mplgaming.com>
2022-10-16 22:47:21 +0530

    code

--- Applications/CarExample/app/src/main/java/com/droid/code/Demo.java
+++ Applications/CarExample/app/src/main/java/com/droid/code/Demo.java
@@ -9,18 +9,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.droid.code.components.CarComponent;
 import com.droid.code.components.DaggerCarComponent;
 import com.droid.code.databinding.ActivityMainBinding;
+import com.droid.code.structures.Car;
+
+import javax.inject.Inject;
 
 public class Demo extends AppCompatActivity {
 
     public static final String PROJECT_TAG = "Vehicle_Demo";
-
     private ActivityMainBinding binding;
+
+    // @Inject Car car;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView( binding.getRoot());
+
+        // Field injection
+        // DaggerCarComponent.builder().build().inject(this);
+
         binding.btnInitiateId.setOnClickListener(v -> {
             CarComponent component = DaggerCarComponent.create();
             component.getCarInstance().drive();
--- Applications/CarExample/app/src/main/java/com/droid/code/components/CarComponent.java
+++ Applications/CarExample/app/src/main/java/com/droid/code/components/CarComponent.java
@@ -1,10 +1,14 @@
 package com.droid.code.components;
 
+import com.droid.code.Demo;
 import com.droid.code.structures.Car;
 
 import dagger.Component;
 
 @Component
 public interface CarComponent {
+    // For:-> Constructor Injection
     Car getCarInstance();
+    // For:-> Variable Injection
+    void inject(Demo instance);
 }
